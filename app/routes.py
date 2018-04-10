0ad640ff581fb70c89083542397da7fc2d0e5039
Joseph Haaga <joseph@Joseph-Dev-MBP-R-344.local>
2018-04-10 15:13:10 -0400

    fix routing bug in register() redirect to login

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
@@ -27,7 +27,8 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
-		if user is None or not user.check_password(form.password.data):
+		#if user is None or not user.check_password(form.password.data):
+		if user == None or not user.check_password(form.password.data):
 			flash('Invalid username or password')
 			return redirect(url_for('login'))	
 		login_user(user, remember=form.remember_me.data)
@@ -42,7 +43,7 @@ def logout():
 	logout_user()
 	return redirect(url_for('index'))
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
 	if current_user.is_authenticated:
 		return redirect(url_for('index'))
@@ -53,16 +54,17 @@ def register():
 		db.session.add(user)
 		db.session.commit()
 		flash("Congratulations! You are now registered")
-		return redirect(url_for('log'))
+		return redirect(url_for('login'))
 	return render_template('register.html', title="Register", form=form)
 
 
 
-
-
-
-
-
-
-
-	
+@app.route('/user/<username>')
+@login_required
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	posts = [
+		{'author': user, 'body': 'Test post #1'},
+		{'author': user, 'body': 'Test post #2'},
+	]
+	return render_template('user.html', user=user, posts=posts)
