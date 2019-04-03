bc0d36d177d5a1a1b09f97ea3e8cc16180f1471e
Joseph Haaga <haaga.joe@gmail.com>
2019-04-02 21:24:18 -0400

    fix vscode pep linting issue breaking order of imports

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -16,18 +17,18 @@ login.login_view = 'login'
 
 from app import routes, models, errors
 
-
 if not app.debug:
-	if not os.path.exists('logs'):
-		os.mkdir('logs')
-	file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
-	file_handler.setFormatter(logging.Formatter(
-		'%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-	file_handler.setLevel(logging.INFO)
-	app.logger.addHandler(file_handler)
-	
-	app.logger.setLevel(logging.INFO)
-	app.logger.info('Microblog statup')
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler(
+        'logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog statup')
 
 '''
 if not app.debug:
