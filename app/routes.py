e78feb975a4e2feb9666c70464c916ad7be23bac
Joseph Haaga <haaga.joe@gmail.com>
2019-04-23 17:24:26 -0400

    import Post model into view function

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from werkzeug.urls import url_parse
 from datetime import datetime
 
